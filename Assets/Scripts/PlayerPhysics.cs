3a7273eb6158e127ba30e1fcc754761133580bce
PedroCeglia <ceglia.pedro@gmail.com>
2022-02-23 18:15:18 -0300

    Add Player Move Function

--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerPhysics : MonoBehaviour
 {
+
+    private CharacterController controller;
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+    }
+    private void Move()
+    {
+        // Get Moviment
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 moviment = new Vector3(horizontal, 0f, vertical);
+        if(moviment.magnitude != 0)
+        {
+            controller.Move(moviment * Time.deltaTime *speed);
+        }
     }
 }
