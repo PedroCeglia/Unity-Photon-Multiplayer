59a808fc839f507fd004cd1b87eed84aec202f88
PedroCeglia <ceglia.pedro@gmail.com>
2022-02-24 18:00:56 -0300

    Initialize Player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    // GameController Singleton
+    public static GameManager Instance { get; private set; }
+    
+    // Initialize Player Atributes
+    [SerializeField] private string _localizacaoPrefab;
+    [SerializeField] private Transform[] _spawnList;
+    
+    // Players List
+    private int _playersNumber = 0;
+    public List<PlayerPhysics> _jogadores;
+    public List<PlayerPhysics> Jogadores { get => _jogadores; private set => _jogadores = value; }
+   
+    // Create a Singleton
+    private void Awake()
     {
-        
+        // Verify if Instance exist
+        if (Instance != null && Instance != this)
+        {
+            // A Instancia Atual Do Game Object é Desabilitada
+            // Usamos a Instancia Anterior
+            gameObject.SetActive(false);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Add a Player
+    private void Start()
     {
-        
+        photonView.RPC("AddPlayer", RpcTarget.AllBuffered);
+        _jogadores = new List<PlayerPhysics>();
+    }
+
+    // Add Player
+    [PunRPC]
+    private void AddPlayer()
+    {
+        _playersNumber++;
+        if(_playersNumber == PhotonNetwork.PlayerList.Length)
+        {
+            CreatePlayer();
+        }
+    }
+
+    // Create Player
+    private void CreatePlayer()
+    {
+        // Create a Player Instance using Photon
+        var playerObj = PhotonNetwork.Instantiate(_localizacaoPrefab, _spawnList[Random.Range(0, _spawnList.Length)].position, Quaternion.identity);
+        var player = playerObj.GetComponent<PlayerPhysics>();
+        // Initialize Player And Pass A Local Player as Parameter
+        player.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
     }
 }
--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
 
-public class PlayerPhysics : MonoBehaviour
+public class PlayerPhysics : MonoBehaviourPunCallbacks
 {
 
+    // Player Move
     private CharacterController controller;
     [SerializeField] private float speed;
+
+    // Player Photon
+    private Player _photonPlayer;
+    private int _id;
+    private bool isMine = false;
+
+    [PunRPC]
+    private void Initialize(Player player)
+    {
+        _photonPlayer = player;
+        _id = _photonPlayer.ActorNumber;
+        GameManager.Instance._jogadores.Add(this);
+        if (photonView.IsMine)
+        {
+            isMine = true;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +37,10 @@ public class PlayerPhysics : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Move();
+        if (isMine)
+        {
+            Move();
+        }
     }
     private void Move()
     {
